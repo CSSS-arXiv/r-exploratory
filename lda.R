2f745c87e0da7ca23a24afa8fa042294770f89b2
bschneeman <bschneeman@homeaway.com>
2015-06-14 17:50:38 -0600

    tokenization, 50 topics, actually doing topic modelling

--- lda.R
+++ lda.R
@@ -40,14 +40,15 @@ if(file.access(sample_documents_file)){
   load(sample_documents_file)
 }
 
-train_documents_df = sample_documents_df %>% sample_n(100)
+train_documents_df = sample_documents_df %>% sample_n(200)
 
 mallet.instances <- mallet.import(train_documents_df$name, 
                                   train_documents_df$content, 
-                                  stoplist.file="./resources/stopwords.txt")
+                                  stoplist.file="./resources/stopwords.txt",
+                                  token.regexp = "[a-zA-Z]+")
 
 #create topic trainer object. 10 Topics
-n.topics <- 10
+n.topics <- 50
 topic.model <- MalletLDA(n.topics)
 
 #load documents
@@ -57,3 +58,58 @@ topic.model$loadDocuments(mallet.instances)
 ## These may be useful in further curating the stopword list.
 vocabulary <- topic.model$getVocabulary()
 word.freqs <- mallet.word.freqs(topic.model)
+
+## Optimize hyperparameters every 20 iterations,
+## after 50 burn-in iterations.
+topic.model$setAlphaOptimization(20, 50)
+
+## Now train a model. Note that hyperparameter optimization is on, by default.
+## We can specify the number of iterations. Here we'll use a large-ish round number.
+topic.model$train(200)
+
+## NEW: run through a few iterations where we pick the best topic for each token,
+## rather than sampling from the posterior distribution.
+topic.model$maximize(10)
+
+## Get the probability of topics in reviews and the probability of words in topics.
+## By default, these functions return raw word counts. Here we want probabilities,
+## so we normalize, and add "smoothing" so that nothing has exactly 0 probability.
+doc.topics <- mallet.doc.topics(topic.model, smoothed=T, normalized=T)
+topic.words <- mallet.topic.words(topic.model, smoothed=T, normalized=T)
+
+# from http://www.cs.princeton.edu/~mimno/R/clustertrees.R
+## transpose and normalize the doc topics
+topic.docs <- t(doc.topics)
+topic.docs <- topic.docs / rowSums(topic.docs)
+write.csv(topic.docs, "dcb-topic-docs.csv")
+
+## Get a vector containing short names for the topics
+topics.labels <- rep("", n.topics)
+for (topic in 1:n.topics) {
+  topics.labels[topic] <- paste(mallet.top.words(topic.model, topic.words[topic,], num.top.words=5)$words, collapse=" ")
+}
+# have a look at keywords for each topic
+topics.labels
+
+# create data.frame with columns as reviews and rows as topics
+topic_docs <- data.frame(topic.docs)
+names(topic_docs) <- rownames(reviews)
+
+docs_topics = t(topic_docs)
+best_fits = tail(sort(docs_topics[,3]))
+best_fit = names(best_fits)[length(best_fits)]
+train_documents_df[best_fit,"contents"]
+
+distance = dist(topic.words, diag=T)
+print(distance, digits=3)
+
+## cluster based on shared words
+filename = paste("./plots/dendrogram", nrow(train_documents_df), "png", sep=".")
+titleText = paste0(nrow(train_documents_df), " Document Topic Dendrogram")
+
+png(file=filename, bg="white", height=1050, width=1050)
+plot(hclust(distance), 
+     labels=topics.labels, font=2, lwd=3, cex=2, main="")
+title(main=titleText, cex.main = 4)
+dev.off()
+
